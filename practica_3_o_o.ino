444add12f833591072fe76161a349d41aa84f636
Ethan04Munoz <ethanmunozlopez04@gmail.com>
2022-08-27 19:28:08 -0500

    Ya funciona teclado lcd y giros, pero ocurre un pequeño bug con el teclado mientras no ponemos un caracter

--- practica_3_o_o.ino
+++ practica_3_o_o.ino
@@ -11,8 +11,8 @@ LiquidCrystal_I2C lcd(0x27,16,2);  //
 #include <Keypad.h>
 const byte filas = 4; 
 const byte columnas = 4;
-byte pinesFilas[]  = {9,8,7,6};
-byte pinesColumnas[] = {5,4,3,2};
+byte pinesFilas[]  = {10,9,8,7};
+byte pinesColumnas[] = {6,5,4,3};
 char teclas[4][4] = {{'1','2','3','A'},
                      {'4','5','6','B'},
                      {'7','8','9','C'},
@@ -20,68 +20,109 @@ char teclas[4][4] = {{'1','2','3','A'},
 Keypad teclado1 = Keypad( makeKeymap(teclas), pinesFilas, pinesColumnas, filas, columnas); 
 
 Servo servo1;
-char angle=200;
+//char angle=200;
 void setup() {
   servo1.attach(pservo);
   servo1.write(0);
   Serial.begin(9600);
+  // Inicializar el LCD
+  lcd.init();
+  //Encender la luz de fondo.
+  lcd.backlight();
 }
 
 void loop() {
-  while(Serial.available()==0){
+  /*while(Serial.available()==0){
   }
   angle=Serial.read();
-  Serial.println(angle);
+  Serial.println(angle);*/
+  /*lcd.setCursor(0,0);
+  lcd.print("Ingrese el ang");
+  lcd.setCursor(0,1);
+  lcd.print("ulo del 0 al 9:");
+  while(true){
+      angle = teclado1.getKey();
+      break;
+  }*/
+  char angle = teclado1.getKey();
+  while(angle == NO_KEY)
+  {
+    angle = teclado1.getKey();
+    lcd.print(angle);
+    delay(100); 
+  }
+  lcd.clear();
   switch(angle){
     case '0':
     {
+      lcd.setCursor(0,0);
+      lcd.print("Angulo 0: 0 grados");
       servo1.write(0);
       break;
     }
     case '1':
     {
+      lcd.setCursor(0,0);
+      lcd.print("Angulo 1: 20 grados");
       servo1.write(20);
       break;
     }
     case '2':
     {
+      lcd.setCursor(0,0);
+      lcd.print("Angulo 2: 40 grados");
       servo1.write(40);
       break;
     }
     case '3':
     {
+      lcd.setCursor(0,0);
+      lcd.print("Angulo 3: 60 grados");
       servo1.write(60);
       break;
     }
     case '4':
-    {
+    {      
+      lcd.setCursor(0,0);
+      lcd.print("Angulo 4: 80 grados");
       servo1.write(80);
       break;
     }
     case '5':
-    {
+    {      
+      lcd.setCursor(0,0);
+      lcd.print("Angulo 5: 100 grados");
       servo1.write(100);
       break;
     }
     case '6':
-    {
+    {      
+      lcd.setCursor(0,0);
+      lcd.print("Angulo 6: 120 grados");
       servo1.write(120);
       break;
     }
     case '7':
     {
+      lcd.setCursor(0,0);
+      lcd.print("Angulo 7: 140 grados");
       servo1.write(140);
       break;
     }
     case '8':
-    {
+    {      
+      lcd.setCursor(0,0);
+      lcd.print("Angulo 8: 160 grados");
       servo1.write(160);
       break;
     }
     case '9':
     {
+      lcd.setCursor(0,0);
+      lcd.print("Angulo 9: 180 grados");
       servo1.write(180);
       break;
     }
+    delay(2500);
   }
 }
